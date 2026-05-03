0f9a531921c7dec7174f1492205aebce70d9f3b4
Igor Luciano <igorlucianocp@gmail.com>
2026-05-02 21:31:12 -0300

    Make prototype result status bar transparent

--- app/src/main/java/igorluciano/com/br/combustivelflex/NewResultActivity.java
+++ app/src/main/java/igorluciano/com/br/combustivelflex/NewResultActivity.java
@@ -2,7 +2,10 @@ package igorluciano.com.br.combustivelflex;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -17,6 +20,7 @@ public class NewResultActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setupTransparentStatusBar();
         setContentView(R.layout.activity_new_result);
 
         TextView resultText = findViewById(R.id.new_result_text);
@@ -70,6 +74,14 @@ public class NewResultActivity extends Activity {
 
     private boolean hasConsumptionValues(double gasolineConsumption, double ethanolConsumption) {
         return gasolineConsumption > 0 && ethanolConsumption > 0;
+    }
+
+    private void setupTransparentStatusBar() {
+        Window window = getWindow();
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
     }
 
     private int calculateByConsumption(
